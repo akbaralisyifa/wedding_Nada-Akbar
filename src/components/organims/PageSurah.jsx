7b2846fe93301c6de0814156758202d4c7ea7814
akbaralisyifa <akbaralisyifa22@gmail.com>
2024-02-23 21:06:20 +0700

    revisi Surah An-Nur

--- src/components/organims/PageSurah.jsx
+++ src/components/organims/PageSurah.jsx
@@ -2,7 +2,7 @@ import BackgroundImage from "../atoms/BackgroundImage"
 import Button from "../atoms/Button"
 import Layout from "../atoms/Layout"
 import Image from "../atoms/Image"
-import { BG2, BISMILLAH, SURAH } from "../../assets"
+import { BG2, BISMILLAH } from "../../assets"
 
 const PageSurah = () => {
   return (
@@ -14,7 +14,8 @@ const PageSurah = () => {
 
             <p className="my-5">---------------------------</p>
 
-            <Image image={SURAH} style={"w-[400px]"}/>
+            {/* <Image image={SURAH} style={"w-[400px]"}/> */}
+            <p className="text-3xl text-center px-4">وَمِنْ اٰيٰتِهٖٓ  اَنْ خَلَقَ لَكُمْ مِّنْ اَنْفُسِكُمْ اَزْوَاجًا لِّتَسْكُنُوْٓا اِلَيْهَا وَجَعَلَ بَيْنَكُمْ مَّوَدَّةً وَّرَحْمَةً ۗاِنَّ فِيْ ذٰلِكَ لَاٰيٰتٍ لِّقَوْمٍ يَّتَفَكَّرُوْنَ</p>
             <p className="my-5">---------------------------</p>
 
             <blockquote className="px-4 text-sm font-medium italic text-center font-montserrat"> Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan di antaramu rasa kasih dan sayang. Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir.</blockquote>
