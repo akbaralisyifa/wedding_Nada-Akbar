5b5c5291caaf0615f2e26ff805144bf0ff9cccb0
akbaralisyifa <akbaralisyifa22@gmail.com>
2024-02-23 13:39:56 +0700

    Revisi Length Pesan

--- src/components/molecules/CardPesan.jsx
+++ src/components/molecules/CardPesan.jsx
@@ -4,7 +4,7 @@ const CardPesan = ({data}) => {
   return (
     <div className=" w-[300px] h-[300px] shadow-md rounded-md bg-colorNav/50 p-4">
         <h1 className="font-semibold text-pink-950 truncate">{data.name}</h1>
-        <p className=" pt-3 font-alice truncate">{data.pesan}</p>
+        <p className=" pt-3 font-alice line-clamp-5">{data.pesan}</p>
     </div>
   )
 }
